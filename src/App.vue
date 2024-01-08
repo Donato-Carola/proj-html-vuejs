d5c29210d90072d628750bc42208455b573dc3f0
Donato-Carola <don94@live.it>
2024-01-08 11:15:41 +0100

    componenti principali per lo svolgimento

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
+<template>
+ <AppHeader/>
+ <AppMain/>
+ <AppFooter/>
+</template>
+
+
+
 <script >
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default{
+  components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+  },
+
+
+
+  data() {
+    return {
+      
+    }
+  },
+}
+
+
+
 
 </script>
 
-<template>
- ciao
-</template>
+
 
 <style lang="scss" >
 @use "./styles/general.scss"
