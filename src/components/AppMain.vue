1461a551fd198907c4cf874ca0af84695603ed8d
Donato-Carola <don94@live.it>
2024-01-08 19:03:30 +0100

    modifiche appmain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,42 @@
-<template >
-    <div>
-        MAIN
-    </div>
+<template>
+  <main>
+    <section>
+      <article>
+        <h1>Welcome to Avada Health</h1>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo</p>
+        
+         
+        
+
+      </article>
+    </section>
+  </main>
 </template>
+
 <script>
+
+
+
+
 export default {
-    data() {
-        return {
-            
-        }
-    },
-}
+
+  data() {
+    return {
+       
+
+        
+    };
+  },
+};
 </script>
+
 <style lang="scss" scoped>
-    
+@use '../styles/partials/mixins' as *;
+section{
+   @include center-content;
+   padding: 6rem 2rem 0.5rem 2rem ;
+   text-align: center;
+}
+
+
 </style>
