773a132d11f6435b086b2e6141f949c038256e06
Donato-Carola <don94@live.it>
2024-01-08 19:57:56 +0100

    sviluppo idea principale usando props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,28 +1,51 @@
 <template>
   <main>
     <section>
-      <article>
+
+
+
+      <article class="firstArticle">
+        <div class="titleArticle">
         <h1>Welcome to Avada Health</h1>
         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo</p>
+      </div>
+     <div class="row">
+         <firstCardMain v-for="card in cards" 
+         :key=card.id
+         :card="card"
+         />
+        </div>   
         
-         
-        
-
       </article>
+
+
+
+
+
     </section>
   </main>
 </template>
 
 <script>
 
-
+import firstCardMain from './firstCardMain.vue';
 
 
 export default {
-
+components:{
+    firstCardMain
+},
   data() {
     return {
-       
+     
+
+
+        cards: [
+        { id: 1, title: "same day appointments", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium ", image: "/img/icon-1.png" },
+        { id: 2, title: "world class facilities", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium ", image: "/img/icon-2.png" },
+        { id: 3, title: "expert doctors", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium ", image: "/img/icon-3.png" },
+        { id: 4, title: "complementary therapies", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium ", image: "/img/icon-4.png" }
+      ]
 
         
     };
@@ -34,8 +57,11 @@ export default {
 @use '../styles/partials/mixins' as *;
 section{
    @include center-content;
-   padding: 6rem 2rem 0.5rem 2rem ;
-   text-align: center;
+  padding-top: 5rem;
+
+}
+div.titleArticle{
+    padding: 0 8rem;
 }
 
 
---
+++ src/components/firstCardMain.vue
@@ -0,0 +1,31 @@
+<template >
+   
+  <div class="card border-0 col-lg-3 flex-nowrap d-flex align-items-center" >
+  <img :src="card.image" class="" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{{card.title}}</h5>
+    <p class="card-text">{{card.content}}</p>
+    
+  </div>
+</div>
+
+</template>
+<script>
+export default {
+    props:{
+        card:{
+            type:Object,
+            required:true
+        }
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+  img{
+    object-fit: cover;
+    width: 70px;
+    height: 70px;
+    
+  }
+</style>
